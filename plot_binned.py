modified:   planck_correlate_all_sky_data.py '' 	modified:   plot_binned.py  'corrected units to microK^2/m^4' 	deleted:    session.py

plot_binned.py
@@ -8,7 +8,7 @@ def plotBinned(cls_in,dcls_in,l_out,bins,output_prefix,title=None,theory=None,dt
 	if not (theory is None) :
 		plt.plot(l_out,theory,'r-')	
 	if not (dtheory is None) :
-		plt.fill_between(l_out,(theory-cosmic),(theory+cosmic),alpha=.5,facecolor='green')
+		plt.fill_between(l_out,(theory-dtheory),(theory+dtheory),alpha=.5,facecolor='red')
 	if not (cosmic is None) :
 		plt.errorbar(l_out,theory,yerr=dtheory,color='red')
 	plt.errorbar(l_out,cls_in,yerr=dcls_in,xerr=bins,color='black',fmt='k.',linestyle='None')
@@ -17,7 +17,7 @@ def plotBinned(cls_in,dcls_in,l_out,bins,output_prefix,title=None,theory=None,dt
 	plt.xlim([0,np.max(l_out+bins)])
 	#plt.ylim([np.min(b_cl['llcl']-b_dcl['llcl']),np.max(b_cl['llcl']+b_dcl['llcl'])])
 	plt.xlabel('$\ell$')
-	plt.ylabel('$\\frac{\ell(\ell+1)}{2\pi}C_{\ell}\ \\frac{\mu K^{2}rad}{m^{4}}$')
+	plt.ylabel('$\\frac{\ell(\ell+1)}{2\pi}C_{\ell}\ \\frac{\mu K^{2}}{m^{4}}$')
 	if title:
 		plt.title(title)
 	else: